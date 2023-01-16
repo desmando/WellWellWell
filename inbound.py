4f755bfdd8ec0dcc38c1d030be05bd5d13c5f019
desmando <mike@combatmedic.net>
2023-01-16 10:59:09 -0700

    Correct query

--- inbound.py
+++ inbound.py
@@ -9,7 +9,7 @@ sqliteConnection = sqlite3.connect('sql.db')
 cursor = sqliteConnection.cursor()
 query = 'CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY AUTOINCREMENT, dtg int, state text, device text);'
 cursor.execute(query)
-query = 'CREATE TABLE IF NOT EXISTS readings (id INTEGER PRIMARY KEY AUTOINCREMENT, device text, on int, off int, duration int);'
+query = 'CREATE TABLE IF NOT EXISTS readings (id INTEGER PRIMARY KEY AUTOINCREMENT, device text, onTime int, offTime int, duration int);'
 cursor.execute(query)
 
 class MyServer(BaseHTTPRequestHandler):
@@ -20,12 +20,18 @@ class MyServer(BaseHTTPRequestHandler):
         parse_result = urlparse(self.path)
         dict_result = parse_qs(parse_result.query)
         self.wfile.write(bytes("<html>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         if (parse_result.path=="/write"):
-                query = "insert into readings(dtg, state, device) values ("+str(int(time.time()))+", '"+dict_result['state'][0]+"', '"+dict_result['device'][0]+"');"
+                query = "insert into logs(dtg, state, device) values ("+str(int(time.time()))+", '"+dict_result['state'][0]+"', '"+dict_result['device'][0]+"');"
                 cursor.execute(query)
                 sqliteConnection.commit()
-                self.wfile.write(bytes(query, "utf-8"))
+                if ( dict_result['state'][0]=="pumpOn" ):
+                    query = "insert into readings(device, onTime) values ("+dict_result['device'][0]+"Pump, "+str(int(time.time()))+");"
+                    cursor.execute(query)
+                    sqliteConnection.commit()
+                if ( dict_result['state'][0]=="floatOn" ):
+                    query = "insert into readings(device, onTime) values ("+dict_result['device'][0]+"Float, "+str(int(time.time()))+");"
+                    cursor.execute(query)
+                    sqliteConnection.commit()
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
